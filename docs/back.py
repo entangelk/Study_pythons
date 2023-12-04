9ec4f641fe97657d00d04bba203b656e064e031d
entangelk <kdtyohan@gmail.com>
2023-12-04 15:52:13 +0900

    quest

--- docs/back.py
+++ docs/back.py
@@ -1,23 +1,5 @@
-a,b = map(int, input().split())
-c = int(input())
 
-d = (b+c)//60
-print(d)
-
-if (b+c) < 60:
-        print(a, b+c)
-        pass
-elif (b+c)%60 == 0:
-    if a == 23:
-        print('0 0')
-        pass
-    else :
-        print(a+d,'0')
-        pass        
-else :
-    if a == 23:
-        print('0', (b+c)-60)
-        pass
-    else :
-        print(a+1, (b+c)-60)
-        pass
+t = int(input())
+for i in range(t):
+    a,b = map(int, input().split())    
+    print(a+b)
