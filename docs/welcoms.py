5ae893b814704c9081f66d9d4b2278a81aadccea
entangelk <kdtyohan@gmail.com>
2023-12-04 12:49:51 +0900

    first commit

---
+++ docs/welcoms.py
@@ -0,0 +1,14 @@
+#문자 출력
+helloworld = "hellow, world! yohan."
+print(helloworld)
+
+#숫자 합산 출력
+numbers = 3+5
+print(numbers)
+
+# 데이터 타입 : 문자형 or 숫자형 등 변수에 대한 정의
+
+# 가정문
+if 5>2:
+    print('Five is greater than two!')
+
